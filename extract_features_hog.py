ef5a9ba135646b18ef3c01d2ffc9f596cea416bb
Pedro Bianchini de Quadros <pedrobiqua@outlook.com>
2026-05-05 11:32:56 -0300

    adicionando extração usando hog e avaliação do modelo separados

---
+++ extract_features_hog.py
@@ -0,0 +1,84 @@
+from skimage.feature import hog
+from skimage.io import imread
+from skimage.color import rgb2gray
+import csv
+import numpy as np
+import os
+
+#### Organização dos datasets
+DATA_DIR = "/home/pedro/Datasets/FER"
+TRAIN_DIR = os.path.join(DATA_DIR, "train")
+TEST_DIR = os.path.join(DATA_DIR, "test")
+
+OUTPUT_DIR = "features_csv"
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+SEED = 24
+
+def extract_features(dataset_path):
+    features = []
+    labels = []
+
+    for label in os.listdir(dataset_path):
+        label_path = os.path.join(dataset_path, label)
+
+        for img_name in os.listdir(label_path):
+            img_path = os.path.join(label_path, img_name)
+
+            # lê imagem
+            image = imread(img_path)
+
+            # converte para grayscale (HOG normalmente usa isso)
+            if len(image.shape) == 3:
+                image = rgb2gray(image)
+
+            # extrai HOG
+            feat = hog(
+                image,
+                orientations=9,
+                pixels_per_cell=(8, 8),
+                cells_per_block=(2, 2),
+                block_norm='L2-Hys',
+                visualize=False
+            )
+
+            features.append(feat)
+            labels.append(label)
+
+    return np.vstack(features), np.hstack(labels)
+
+
+# --------------------------------------------------
+# CSV writer
+# --------------------------------------------------
+def save_csv(features, labels, filename):
+    num_features = features.shape[1]
+
+    header = ["label"] + [f"f{i}" for i in range(num_features)]
+
+    with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+
+        for x, y in zip(features, labels):
+            writer.writerow([y] + x.tolist())
+
+# --------------------------------------------------
+# Extract + Save (Using Model)
+# --------------------------------------------------
+X_train, y_train = extract_features(TRAIN_DIR)
+X_test, y_test = extract_features(TEST_DIR)
+
+print("Train features shape:", X_train.shape)
+print("Test features shape:", X_test.shape)
+
+save_csv(
+    X_train,
+    y_train,
+    os.path.join(OUTPUT_DIR, "train_features_hog.csv")
+)
+
+save_csv(
+    X_test,
+    y_test,
+    os.path.join(OUTPUT_DIR, "test_features_hog.csv")
+)
---
+++ model_evaluate.py
@@ -0,0 +1,169 @@
+import os
+import pandas as pd
+from sklearn.model_selection import cross_val_score, StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, \
+    classification_report
+
+from sklearn.pipeline import Pipeline
+
+### Modelos que serão avaliados
+from sklearn.neural_network import MLPClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+from sklearn.model_selection import GridSearchCV
+
+# # -------------------------
+# # Path do dataset para ser avaliado nos modelos
+# # -------------------------
+PATH = "/home/pedro/Datasets/FER"
+TRAIN = PATH + "/train_features_model.csv"
+TEST = PATH + "/test_features_model.csv"
+
+def train_and_evaluate(X_train, y_train, X_test, y_test, model, param_grid):
+    pipeline = Pipeline([
+        ('scaler', StandardScaler()),
+        ('model', model)
+    ])
+    param_grid = {
+        f"model__{key}": value for key, value in param_grid.items()
+    }
+    skf = StratifiedKFold(
+        n_splits=5,
+        shuffle=True,
+        random_state=42
+    )
+
+    ## Grid search usando o pipeline, utiliza o scaler de maneira correta
+    grid = GridSearchCV(
+        estimator=pipeline,
+        param_grid=param_grid,
+        cv=skf,
+        scoring='f1_macro',
+        n_jobs=-1
+    )
+
+    grid.fit(X_train, y_train)
+
+    print("\nMelhores parâmetros encontrados:")
+    print(grid.best_params_)
+
+    print("Score médio (CV):")
+    print(grid.best_score_)
+
+    best_model = grid.best_estimator_
+
+    y_pred = best_model.predict(X_test)
+    report = classification_report(y_test, y_pred)
+
+    result_text = ""
+    result_text += "Parâmetros:\n"
+    result_text += str(grid.best_params_) + "\n\n"
+
+    result_text += "Score médio (CV):\n"
+    result_text += str(grid.best_score_) + "\n\n"
+
+    result_text += "Reports:\n"
+    result_text += report + "\n"
+
+    return result_text
+
+
+models_and_parameters = [
+    # {
+    #     "name": "LogisticRegression",
+    #     "model": LogisticRegression(max_iter=1000),
+    #     "params": {
+    #         "C": [0.01, 0.1, 1, 10]
+    #     }
+    # },
+    #
+    # {
+    #     "name": "KNN",
+    #     "model": KNeighborsClassifier(),
+    #     "params": {
+    #         "n_neighbors": [3, 5, 7, 9]
+    #     }
+    # },
+    # {
+    #     "name": "SVM",
+    #     "model": SVC(),
+    #     "params": {
+    #         "C": [0.1, 1, 10],
+    #         "kernel": ["rbf", "linear"]
+    #     }
+    # },
+    # {
+    #     "name": "MLP",
+    #     "model": MLPClassifier(max_iter=1000),
+    #     "params": {
+    #         "hidden_layer_sizes": [(50,), (50, 50)],
+    #         "alpha": [0.0001, 0.001]
+    #     }
+    # },
+    # {
+    #     "name": "RandomForest",
+    #     "model": RandomForestClassifier(),
+    #     "params": {
+    #         "n_estimators": [100, 200],
+    #         "max_depth": [None, 5, 10]
+    #     }
+    # },
+    {
+        "name": "DecisionTree",
+        "model": DecisionTreeClassifier(),
+        "params": {
+            "max_depth": [None, 5, 10]
+        }
+    },
+    # {
+    #     "name": "NaiveBayes",
+    #     "model": GaussianNB(),
+    #     "params": {
+    #         # vazio mesmo
+    #     }
+    # },
+    # {
+    #     "name": "LDA",
+    #     "model": LinearDiscriminantAnalysis(),
+    #     "params": {
+    #         # vazio mesmo
+    #     }
+    # }
+]
+
+DATASET = TRAIN.split("/")[-1]
+
+# Carregar dados
+X_train = pd.read_csv(TRAIN)
+y_train = X_train['label']
+X_train = X_train.drop('label', axis=1)
+
+X_test = pd.read_csv(TEST)
+y_test = X_test['label']
+X_test = X_test.drop('label', axis=1)
+
+OUTPUT_DIR = "results"
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+print("TESTE DE CLASSIFICADORES")
+print(f"{DATASET}\n")
+
+for item in models_and_parameters:
+    print("\nMODELO:", item["name"])
+
+    result_text = train_and_evaluate(
+        X_train, y_train,
+        X_test, y_test,
+        item["model"],
+        item["params"]
+    )
+
+    file_path = os.path.join(OUTPUT_DIR, f"{item['name']}.txt")
+    with open(file_path, "w") as f:
+        f.write(result_text)
